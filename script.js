2c2da287e9f74932d65b55c54ffee8abf0e8dfff
Wendy C <chriscowendy@gmail.com>
2020-11-17 14:20:02 -0800

    working on image pop-out

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function changeText() {
 }
 
 changeText();
+
+function imageEnlarge(image) {
+    document.getElementById(image).style.height='50%';
+    document.getElementById(image).style.width='50%';
+    document.getElementById(image).style.position='relative';
+}
+
+function imageReset() {
+    document.getElementById('design').style.height ="auto";
+    document.getElementById('design').style.width='auto';
+    document.getElementById('design').style.position="relative";
+}
